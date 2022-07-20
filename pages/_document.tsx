7cec61057f2fa31b9e49802e3acedbdf65f02287
Jaepang <jake_king@kakao.com>
2022-07-21 00:37:29 +0900

    add favicon and meta og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="/static/favicon.ico" />
           <link href="https://hangeul.pstatic.net/hangeul_static/css/maru-buri.css" rel="stylesheet" type="text/css" />
+          <meta property="og:title" content="Begin Again" />
+          <meta property="og:description" content="분당소망교회 청년부 여름신앙수련회 활동 페이지" />
+          <meta property="og:url" content="https://bdsm-begin-again.vercel.app" />
+          <meta property="og:image" content="https://bdsm-begin-again.vercel.app/thumbnail.png" />
         </Head>
         <body>
           <Main />
